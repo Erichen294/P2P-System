aa1f04d4c7dc3afd969e9fe1b351abe53ff18326
Eric Chen <chene@bu.edu>
2024-03-30 13:58:18 -0400

    added forceful exit when ctrl-c

--- receiver.py
+++ receiver.py
@@ -1,14 +1,18 @@
 import networkClass
 import threading
 import sys
+import os
 
 # Flag to track if a message has been received
 message_received = False
 
+# Flag to indicate if threads should stop
+stop_threads = False
+
 def receive_messages():
-    global message_received
+    global message_received, stop_threads
     try:
-        while True:
+        while not stop_threads:
             message = networkClass.receive_message(65435)
             message_received = True
             if message:
@@ -18,20 +22,26 @@ def receive_messages():
                 print(f"{message.sender}: {message.text}\nUser2: ", end='')
                 message_received = False
     except KeyboardInterrupt:
+        pass  # Let the thread exit gracefully without printing
+    finally:
         print("Disconnecting...")
-        sys.exit()
+        os._exit(0)  # Exit the thread forcefully
 
 def send_messages():
-    global message_received
+    global message_received, stop_threads
     try:
-        while True:
+        while not stop_threads:
             message = input("User2: ")
             # Clear the flag before sending a message
             message_received = False
             network.send_message(networkClass.Peer("Receiver", "127.0.0.1", 65434), "User2", message)
     except KeyboardInterrupt:
+        pass  # Let the thread exit gracefully without printing
+    except EOFError:
+        pass  # Let the thread exit gracefully without printing
+    finally:
         print("Disconnecting...")
-        sys.exit()
+        os._exit(0)  # Exit the thread forcefully
 
 if __name__ == "__main__":
     # Create sender
@@ -57,5 +67,5 @@ if __name__ == "__main__":
         receive_thread.join()
         send_thread.join()
     except KeyboardInterrupt:
-        print("Disconnecting...")
-        sys.exit()
+        stop_threads = True  # Set the flag to stop threads
+        os._exit(0)  # Exit the main thread forcefully
